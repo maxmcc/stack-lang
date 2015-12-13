a294873ee20da635cb67324301fb8efb1de9bb8b
Meyer Kizner <meyer@pre-alpha.com>
2015-12-13 04:27:53 -0500

    Tack on a freshen function, can remove later if it doesn't pan out

--- Inference.hs
+++ Inference.hs
@@ -9,7 +9,6 @@ import Terms
 import Data.Char (toUpper)
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Text.Printf
 import Control.Monad.Writer
 import Control.Monad.State
 import Control.Monad.Except
@@ -27,6 +26,57 @@ freshVVar = state ((,) <*> succ)
 
 freshSVar :: TC TypeVariable
 freshSVar = toUpper <$> state ((,) <*> succ)
+
+type FreshenTC a =
+  StateT (Map TypeVariable TypeVariable)
+  (WriterT [Constraint]
+  (StateT TypeVariable
+  (Either String)))
+  a
+
+freshenVVars :: ValueType -> FreshenTC ValueType
+freshenVVars VIntTy      = return VIntTy
+freshenVVars VBoolTy     = return VBoolTy
+freshenVVars (VListTy t) = VListTy <$> freshenVVars t
+freshenVVars (VFuncTy t) = VFuncTy <$> freshenVVarsFunc t
+freshenVVars (VVarTy a)  =
+  do a' <- lift freshVVar
+     ctxt <- get
+     let newVar = Map.findWithDefault a' a ctxt
+     let ctxt' = Map.insert a newVar ctxt
+     put ctxt'
+     return $ VVarTy newVar
+
+freshenVVarsFunc :: FuncType -> FreshenTC FuncType
+freshenVVarsFunc (F (S a s) (S b t)) =
+  do s' <- mapM freshenVVars s
+     t' <- mapM freshenVVars t
+     return $ F (S a s') (S b t')
+
+freshenSVars :: ValueType -> FreshenTC ValueType
+freshenSVars VIntTy      = return VIntTy
+freshenSVars VBoolTy     = return VBoolTy
+freshenSVars (VListTy t) = VListTy <$> freshenSVars t
+freshenSVars (VFuncTy t) = VFuncTy <$> freshenSVarsFunc t
+freshenSVars (VVarTy a)  = return $ VVarTy a
+
+freshenSVarsFunc :: FuncType -> FreshenTC FuncType
+freshenSVarsFunc (F (S a s) (S b t)) =
+  do a' <- lift freshSVar
+     b' <- lift freshSVar
+     ctxt <- get
+     let newAVar = Map.findWithDefault a' a ctxt
+     let newBVar = Map.findWithDefault b' b ctxt
+     let ctxt' = Map.insert b newBVar (Map.insert a newAVar ctxt)
+     put ctxt'
+     s' <- mapM freshenSVars s
+     t' <- mapM freshenSVars t
+     return $ F (S newAVar s') (S newBVar t')
+
+freshen :: FuncType -> TC FuncType
+freshen t =
+  do t' <- evalStateT (freshenVVarsFunc t) Map.empty
+     evalStateT (freshenSVarsFunc t') Map.empty
 
 runTC :: TC a -> Either String (a, [Constraint])
 runTC m = evalStateT (runWriterT m) 'm'
@@ -62,7 +112,7 @@ inferType _ IdTerm =
 
 inferType c (CatTerm t u) =
   do F (S a1 t1) (S b1 u1) <- inferType c t
-     F (S a2 t2) (S b2 u2) <- inferType c u
+     F (S a2 t2) (S b2 u2) <- inferType c u >>= freshen
      equateSTy (S b1 u1) (S a2 t2)
      return $ F (S a1 t1) (S b2 u2)
 
