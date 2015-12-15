5671e192a6eebf9d2b9ac359521de01a70e65f95
Meyer Kizner <meyer@pre-alpha.com>
2015-12-14 22:40:16 -0500

    Some refactoring and comments in Inference

--- Language.hs
+++ Language.hs
@@ -27,6 +27,15 @@ data Term a
   | PushFuncTerm a (Term a)
     deriving (Eq, Ord, Show)
 
+instance Functor Term where
+  fmap f (IdTerm a)         = IdTerm $ f a
+  fmap f (CatTerm a t1 t2)  = CatTerm (f a) (fmap f t1) (fmap f t2)
+  fmap f (BuiltinTerm a s)  = BuiltinTerm (f a) s
+  fmap f (PushIntTerm a i)  = PushIntTerm (f a) i
+  fmap f (PushBoolTerm a b) = PushBoolTerm (f a) b
+  fmap f (PushNilTerm a)    = PushNilTerm $ f a
+  fmap f (PushFuncTerm a t) = PushFuncTerm (f a) (fmap f t)
+
 extract :: Term a -> a
 extract (IdTerm a)         = a
 extract (CatTerm a _ _)    = a
